07a3dacf6d49302986d7885125c7f845381e06b7
Conrad Hansen-Quartey <Chansenq@gmail.com>
2025-03-17 12:31:23 -0400

    feat: full view standalone overlay

--- src/OuterSpaceComponent.tsx
+++ src/OuterSpaceComponent.tsx
@@ -541,12 +541,31 @@ const TextOverlayTest1 = (props: { topAligned: boolean }) => {
 const TextOverlayTitle = () => {
   return (
     <div style={{ position: 'relative', transform: 'translate(-50%, -30%)',
-      left: '75%', width: '100vw', display: 'flex', justifyContent: 'center' }}>
+      left: '75%', width: '10vw', display: 'flex', justifyContent: 'center' }}>
       <div style={{ display: 'flex' }}>
         {Array(1).fill(null).map((_, index) => (
           <div className="html-overlay" key={index}>
                <h1>Conrad</h1>
               <p>🚀 Welcome to my portfolio!</p>
+           </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+
+const TextOverlayAbout2 = () => {
+  return (
+    <div style={{ position: 'relative', transform: 'translate(-10%, -30%)',
+      left: '75%', margin: '1vw', padding: '1vw',  width: '45vw', display: 'flex', justifyContent: 'center' }}>
+
+      <div style={{ display: 'flex' }}>
+        {Array(1).fill(null).map((_, index) => (
+          <div style={{margin: '0vw', padding: '3vw'}} className="html-overlay" key={index}>
+               <h1>Conrad</h1>
+              <p> My name is Conrad Hansen-Quartey. I live in West Haven, CT. I majored in Engineering Science. </p>
+              <p> With a combination of self-taught and academic training, I have both the passion and engineering skill of a professional software engineer. Having self-taught programming languages throughout high school and learning computer programming design principles and techniques in college, I have become a well-rounded computer scientist. I have a passion not only for developing software, but for solving problems in creative ways. I am deeply committed to using my talents and gifts to serve those around me and society in general.  </p>
            </div>
         ))}
       </div>
@@ -580,7 +599,7 @@ export default function OuterSpaceComponent(props: { windowMinimized: boolean })
       <div
         style={{
           position: 'absolute',
-          top: '50%',
+          top: '80%', //'50%',//'90%',
           // left: '50%',
           transform: 'translate(-50%, -50%)',
           width: 'auto',
@@ -588,9 +607,10 @@ export default function OuterSpaceComponent(props: { windowMinimized: boolean })
         }}
       >
         {/* <TextOverlayTest1 topAligned={true} /> */}
-           <TextOverlayTitle/>
-           <TextOverlayAbout/>
-           <TextOverlayAbout/>
+           {/* <TextOverlayTitle/> */}
+           <TextOverlayAbout2/>
+           {/* <TextOverlayAbout/> */}
+            {/* <TextOverlayAbout/> */}
        </div>
       <Canvas
         style={{
